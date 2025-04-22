7777ac3af7b821bd6197fb86d54639303492b9e4
Mohit Singh <zonicmohit@gmail.com>
2025-04-22 21:17:50 +0530

    commit

--- src/Day1/Q7_GradeRemarks.java
+++ src/Day1/Q7_GradeRemarks.java
@@ -9,10 +9,10 @@ public class Q7_GradeRemarks {
         char grade = Character.toUpperCase(sc.next().charAt(0));
 
         switch (grade) {
-            case 'A': System.out.println("Excellent"); break;
-            case 'B': System.out.println("Very Good"); break;
-            case 'C': System.out.println("Good"); break;
-            case 'D': System.out.println("Pass"); break;
+            case 'A': System.out.println("Very Good"); break;
+            case 'B': System.out.println("Good"); break;
+            case 'C': System.out.println("Ok"); break;
+            case 'D': System.out.println("Bad"); break;
             case 'F': System.out.println("Fail"); break;
             default: System.out.println("Invalid grade");
         }
