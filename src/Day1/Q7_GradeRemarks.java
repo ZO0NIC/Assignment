472c138c1819393ed41614777bf63eb39c67ea0e
Mohit Singh <zonicmohit@gmail.com>
2025-04-22 21:13:02 +0530

    commit

---
+++ src/Day1/Q7_GradeRemarks.java
@@ -0,0 +1,20 @@
+package Day1;
+
+import java.util.Scanner;
+
+public class Q7_GradeRemarks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter grade (A/B/C/D/F): ");
+        char grade = Character.toUpperCase(sc.next().charAt(0));
+
+        switch (grade) {
+            case 'A': System.out.println("Excellent"); break;
+            case 'B': System.out.println("Very Good"); break;
+            case 'C': System.out.println("Good"); break;
+            case 'D': System.out.println("Pass"); break;
+            case 'F': System.out.println("Fail"); break;
+            default: System.out.println("Invalid grade");
+        }
+    }
+}
